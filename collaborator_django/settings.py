f5433c5b848dc563ee74ed594dd90502cac98cfa
reza <rmoslemi1994@gmail.com>
2023-12-05 01:33:56 +0330

    added uml

--- collaborator_django/settings.py
+++ collaborator_django/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     # "django_celery_beat",
     # "django_celery_results",
     "drf_yasg",
