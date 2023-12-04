0faadd8740812053c0fd3125ef9cbb16a926c60a
reza <rmoslemi1994@gmail.com>
2023-12-05 02:17:35 +0330

    fixes

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ python manage.py migrate
 deactivate
 mkdir /run/uwsgi/
 mkdir /run/uwsgi/collaborator_django/
+rm -f /run/uwsgi/collaborator_django/collaborator_django.sock
 mkdir /var/log/uwsgi/
 touch /var/log/uwsgi/collaborator_django.log
 sudo uwsgi --ini uwsgi_config.ini
